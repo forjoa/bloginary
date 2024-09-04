f86243c248d262a21532a3cbd978bb9773829141
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-09-04 23:27:28 +0200

    fix: implement vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "hybrid"
+  output: "hybrid",
+  adapter: vercel()
 });

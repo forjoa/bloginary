7904c72b3ac0bd465475b737cb42bdbc5d2b11b6
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-09-06 11:13:26 +0200

    feat: add colors to categories in home

---
+++ src/lib/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  categories: {
+    "Personal": {
+      font: '#101824',
+      background: '#CDE3FF',
+    },
+    "Profesional": {
+      font: '#172C19',
+      background: '#BBF8C0',
+    },
+  },
+}

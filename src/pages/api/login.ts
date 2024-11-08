07ab16620effc683ff9018e5a6bb8f38f8d8f3ae
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-08 16:29:16 +0100

    build: login endpoint

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,7 +1,27 @@
 import type { APIRoute } from 'astro'
 import { db } from '@/lib/db'
+import bcrypt from 'bcrypt'
 
 // todo: login endpoint
 export const POST: APIRoute = async ({ request, cookies, redirect }) => {
-  return redirect('/dashboard')
+  const body = await request.json()
+  const { email, password } = body
+
+  const result = await db.execute({
+    sql: 'SELECT * FROM author WHERE email = ? ',
+    args: [email],
+  })
+
+  const user = result.rows[0]
+
+  if (!user) return redirect('/subir')
+
+  if (!(await bcrypt.compare(password, user.password as string))) return redirect('/subir')
+
+  cookies.set('userbloginary', user, {
+    path: '/',
+    httpOnly: true,
+  })
+
+  return redirect('/subir')
 }

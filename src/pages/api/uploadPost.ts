ea7429698f3442111d83dcfe00baa44a77700f0d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-09 16:56:59 +0100

    feat: send author id to db

--- src/pages/api/uploadPost.ts
+++ src/pages/api/uploadPost.ts
@@ -1,15 +1,18 @@
 import { db } from '@/lib/db'
+import type { User } from '@/lib/types'
 import type { APIRoute } from 'astro'
 
-// todo: add author id
 export const POST: APIRoute = async ({ request }) => {
+  const encodedUser = request.headers.get('cookie')?.split('userbloginary=')[1]
+  const user = JSON.parse(decodeURIComponent(encodedUser as string)) as User
+
   const body = await request.json()
 
   const { category, title, content } = body
 
   const result = await db.execute({
-    sql: 'INSERT INTO posts (title, content, categoryid) VALUES (?,?,?)',
-    args: [title, content, category],
+    sql: 'INSERT INTO posts (title, content, categoryid, authorid) VALUES (?,?,?,?)',
+    args: [title, content, category, user.id],
   })
 
   return new Response(

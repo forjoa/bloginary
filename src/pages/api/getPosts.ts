a8234f0dc66f877d9ab2b37e6a2f554119a8ea15
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-11 19:22:58 +0100

    refactor: add author name and lastname in general posts

--- src/pages/api/getPosts.ts
+++ src/pages/api/getPosts.ts
@@ -2,7 +2,7 @@ import { db } from '@/lib/db'
 import type { APIRoute } from 'astro'
 
 export const GET: APIRoute = async () => {
-  const result = await db.execute('SELECT * FROM posts')
+  const result = await db.execute('SELECT p.*, a.name, a.lastname FROM posts AS p JOIN author AS a ON p.authorid = a.id')
 
   return new Response(JSON.stringify({ result: result.rows }), {
     headers: { 'content-type': 'application/json' },

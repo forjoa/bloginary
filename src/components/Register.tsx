44ca4d5d19fbcccd50741c8a43858e73e32aa89b
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-11-15 00:41:53 +0100

    build: register ui and logic

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,25 +1,27 @@
 import { useState, type FormEvent } from 'react'
 import { toast, Toaster } from 'sonner'
 
-export default function Login() {
+export default function Register() {
+  const [name, setName] = useState<string>()
+  const [lastname, setLastname] = useState<string>()
   const [email, setEmail] = useState<string>()
   const [password, setPassword] = useState<string>()
 
   const submit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    fetch('/api/login', {
+    fetch('/api/register', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ name, lastname, email, password }),
     })
       .then((res) => res.json())
       .then((data) => {
         if (!data.success) {
           toast.error(data.message)
         } else {
-          window.location.reload()
+          window.location.pathname = '/subir'
         }
       })
   }
@@ -27,10 +29,35 @@ export default function Login() {
     <div className='w-full backdrop-blur-lg border border-zinc-200 p-4 rounded-lg shadow-xl'>
       <Toaster position='top-center' />
       <form className='flex flex-col gap-2' onSubmit={submit}>
-        <h2 className='font-bold text-xl'>Login</h2>
+        <h2 className='font-bold text-xl'>Registro</h2>
         <span className='text-zinc-500'>
-          Gracias por volver a tu lugar de confianza.
+          Gracias por depositar tu confianza con nosotros, podras compartir todo
+          lo que desees.
         </span>
+        <label htmlFor='name' className='flex flex-col gap-1'>
+          Nombre
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type='name'
+            id='name'
+            placeholder='Nombre'
+            autoComplete='off'
+            className='border border-zinc-200 p-4 rounded-lg w-full mb-2'
+          />
+        </label>
+        <label htmlFor='lastname' className='flex flex-col gap-1'>
+          Apellido
+          <input
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+            type='lastname'
+            id='lastname'
+            placeholder='Apellido'
+            autoComplete='off'
+            className='border border-zinc-200 p-4 rounded-lg w-full mb-2'
+          />
+        </label>
         <label htmlFor='email' className='flex flex-col gap-1'>
           Email
           <input
@@ -58,7 +85,7 @@ export default function Login() {
           <input
             type='submit'
             className='flex bg-black text-white items-center justify-center rounded-lg py-2 px-4 cursor-pointer transition-all hover:opacity-50'
-            value='Login'
+            value='Sign up'
           ></input>
         </div>
       </form>
